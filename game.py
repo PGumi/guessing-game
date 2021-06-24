a397b6609ee4ef91f08c82bcb3600a6523abf9a6
PGumi <86422509+PGumi@users.noreply.github.com>
2021-06-24 17:48:27 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,8 @@
 import random
 answer = random.randint(1, 100)
 guess = 0
-counter = 0
+attempt = 0
+while guess != answer:
+    attempt += 1
+    print('This is your ' + str(attempt) + ' attempt')
+    guess = int(input("enter a number between 1 and 100: "))
