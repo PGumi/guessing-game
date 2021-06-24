cb4f5b3ebfa776c2b0038898310639c18cc69d94
PGumi <86422509+PGumi@users.noreply.github.com>
2021-06-24 17:53:10 +0200

    Update game.py

--- game.py
+++ game.py
@@ -6,3 +6,8 @@ while guess != answer:
     attempt += 1
     print('This is your ' + str(attempt) + ' attempt')
     guess = int(input("enter a number between 1 and 100: "))
+    if guess < answer:
+        print('More !')
+    elif guess > answer:
+        print('Fewer !')
+print('It only worked ' + str(attempt) + ' attempt :)')
